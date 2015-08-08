a4386fb9b6cc972d4affaf04b1c0f1572b994cd2
whitewhale6 <whitewhale6@gmail.com>
2015-08-08 14:00:42 +0900

    取得のためのコミット

---
+++ ProjectV/Assets/scripts/WarpByTouch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpByTouch : MonoBehaviour {
+
+	public Vector3 toTransportPosition;
+	public Vector3 fromTransportPosition;
+
+	void OnTriggerExit2D(Collider2D coll){
+		if (coll.gameObject.tag == "Player") {
+			fromTransportPosition = coll.transform.position;
+		}
+	}
+}
