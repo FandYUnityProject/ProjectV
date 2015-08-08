c5f4ee355ba56eb21ed51c07b2fbd0a7676d86fa
whitewhale6 <whitewhale6@gmail.com>
2015-08-08 14:50:10 +0900

    Warp試作

--- ProjectV/Assets/scripts/WarpByTouch.cs
+++ ProjectV/Assets/scripts/WarpByTouch.cs
@@ -5,10 +5,19 @@ public class WarpByTouch : MonoBehaviour {
 
 	public Vector3 toTransportPosition;
 	public Vector3 fromTransportPosition;
+	public Vector3 gapOfMap;
 
-	void OnTriggerExit2D(Collider2D coll){
+	private GameObject player;
+
+	void Start(){
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	void OnTriggerEnter2D(Collider2D coll){
 		if (coll.gameObject.tag == "Player") {
 			fromTransportPosition = coll.transform.position;
+			toTransportPosition = fromTransportPosition + gapOfMap;
+			player.transform.localPosition = toTransportPosition;
 		}
 	}
 }
