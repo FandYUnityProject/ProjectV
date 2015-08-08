769e5a2db7fda26d56968567e2c35bd4f4ffa903
whitewhale6 <whitewhale6@gmail.com>
2015-08-08 22:07:22 +0900

    マップ番地
    マップ番地をデバックに出るようにした

---
+++ ProjectV/Assets/scripts/MapNumberManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapNumberManager : MonoBehaviour {
+
+	public static int mapNumber_X;
+	public static int mapNumber_Y;
+
+	public static void AddMapNumberX(int x){
+		mapNumber_X += x;
+		Debug.Log ("Current Map Number is ("+mapNumber_X +","+ mapNumber_Y+")");
+	}
+	public static void AddMapNumberY(int y){
+		mapNumber_Y += y;
+		Debug.Log ("Current Map Number is ("+mapNumber_X +"," + mapNumber_Y+")");
+	}
+}
