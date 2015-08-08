24449eb258ba385e7ecb22f20d8e02375f74aeed
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-08 09:30:12 +0900

    再スタート
    ignore機能を使用して、開発データを再コミット

---
+++ ProjectV/Assets/scripts/Setting/SaveData/SaveData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveData : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+		// シーン遷移しても削除させない
+		DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
