3e3a0761e3987e16fc505de0b46638655003fe15
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-08 15:02:06 +0900

    BGM関連の追加
    ・仮BGMチーターマンの追加
    ・GameMenu,GameSelectまではBGMを変えず、
    　GameSceneに移動するとBGMが消えるように設定。

---
+++ ProjectV/Assets/scripts/Setting/Sound/SoundScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundScript : MonoBehaviour {
+
+	private static bool isSaveControllObject = false;
+	
+	// saveContoroll(Object)が重複作成されるのを防ぐ
+	void Awake (){
+
+		if (!isSaveControllObject) {
+			
+			// シーン遷移しても削除させない
+			DontDestroyOnLoad (this);
+			isSaveControllObject = true;
+		} else {
+			
+			Destroy(this.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			// ゲームが始まったら削除
+		if (Application.loadedLevelName == "GameScene") {
+			
+			Destroy(this.gameObject);
+		}
+	}
+}
