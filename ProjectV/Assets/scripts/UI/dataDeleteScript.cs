cfaea4e1432a2576894a240067f5cb1912f407d5
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-09 15:10:35 +0900

    セーブデータ削除処理の修正
    ・処理を簡略化。
    ・必要なオブジェクトが足りない場合に警告文を出すように設定。

--- ProjectV/Assets/scripts/UI/dataDeleteScript.cs
+++ ProjectV/Assets/scripts/UI/dataDeleteScript.cs
@@ -5,6 +5,7 @@ public class dataDeleteScript : MonoBehaviour {
 	
 	public GameObject dataButtonObject;
 	dataSelectButtonScript dataSelectButton;
+	string dataNumberName;
 
 	// Use this for initialization
 	void Start () {
@@ -19,56 +20,28 @@ public class dataDeleteScript : MonoBehaviour {
 
 	// ボタンをクリックするとゲームシーンへ移動する
 	public void OnClick() {
+		
+		if( this.name == "Delete01" ){ dataNumberName = "Data01"; }
+		if( this.name == "Delete02" ){ dataNumberName = "Data02"; }
+		if( this.name == "Delete03" ){ dataNumberName = "Data03"; }
 
-		if( this.name == "Delete01" ){
-			PlayerPrefs.SetInt ("Data01isNotNewGame", 0);
-			
-			PlayerPrefs.SetInt("Data01mapNumber_X", 0 );
-			PlayerPrefs.SetInt("Data01mapNumber_Y", 0 );
-			PlayerPrefs.SetString("Data01worldName", "" );
-			PlayerPrefs.SetInt("Data01FUEL_01", 0 );
-			PlayerPrefs.SetInt("Data01FUEL_02", 0 );
-			PlayerPrefs.SetInt("Data01FUEL_03", 0 );
-			PlayerPrefs.SetInt("Data01FUEL_04", 0 );
-			PlayerPrefs.SetInt("Data01FUEL_05", 0 );
-			PlayerPrefs.SetInt("Data01FUEL_06", 0 );
-			PlayerPrefs.SetInt("Data01FUEL_07", 0 );
+		PlayerPrefs.SetInt(dataNumberName + "isNotNewGame", 0);
+		PlayerPrefs.SetInt(dataNumberName + "mapNumber_X", 0 );
+		PlayerPrefs.SetInt(dataNumberName + "mapNumber_Y", 0 );
+		PlayerPrefs.SetString(dataNumberName + "worldName", "" );
+		PlayerPrefs.SetInt(dataNumberName + "FUEL_01", 0 );
+		PlayerPrefs.SetInt(dataNumberName + "FUEL_02", 0 );
+		PlayerPrefs.SetInt(dataNumberName + "FUEL_03", 0 );
+		PlayerPrefs.SetInt(dataNumberName + "FUEL_04", 0 );
+		PlayerPrefs.SetInt(dataNumberName + "FUEL_05", 0 );
+		PlayerPrefs.SetInt(dataNumberName + "FUEL_06", 0 );
+		PlayerPrefs.SetInt(dataNumberName + "FUEL_07", 0 );
 
+		if(dataSelectButton!=null){
 			dataSelectButton.ReData();
+		} else {
+			Debug.LogWarning("Set Game Object!!");
 		}
 
-		if( this.name == "Delete02" ){
-			PlayerPrefs.SetInt ("Data02isNotNewGame", 0);
-			
-			PlayerPrefs.SetInt("Data02mapNumber_X", 0 );
-			PlayerPrefs.SetInt("Data02mapNumber_Y", 0 );
-			PlayerPrefs.SetString("Data02worldName", "" );
-			PlayerPrefs.SetInt("Data02FUEL_01", 0 );
-			PlayerPrefs.SetInt("Data02FUEL_02", 0 );
-			PlayerPrefs.SetInt("Data02FUEL_03", 0 );
-			PlayerPrefs.SetInt("Data02FUEL_04", 0 );
-			PlayerPrefs.SetInt("Data02FUEL_05", 0 );
-			PlayerPrefs.SetInt("Data02FUEL_06", 0 );
-			PlayerPrefs.SetInt("Data02FUEL_07", 0 );
-			
-			dataSelectButton.ReData();
-		}
-
-		if( this.name == "Delete03" ){
-			PlayerPrefs.SetInt ("Data03isNotNewGame", 0);
-			
-			PlayerPrefs.SetInt("Data03mapNumber_X", 0 );
-			PlayerPrefs.SetInt("Data03mapNumber_Y", 0 );
-			PlayerPrefs.SetString("Data03worldName", "" );
-			PlayerPrefs.SetInt("Data03FUEL_01", 0 );
-			PlayerPrefs.SetInt("Data03FUEL_02", 0 );
-			PlayerPrefs.SetInt("Data03FUEL_03", 0 );
-			PlayerPrefs.SetInt("Data03FUEL_04", 0 );
-			PlayerPrefs.SetInt("Data03FUEL_05", 0 );
-			PlayerPrefs.SetInt("Data03FUEL_06", 0 );
-			PlayerPrefs.SetInt("Data03FUEL_07", 0 );
-			
-			dataSelectButton.ReData();
-		}
 	}
 }
