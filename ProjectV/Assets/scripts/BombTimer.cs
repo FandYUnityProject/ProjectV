f9ccf23311c3032a9668806b292db0de0798fc0e
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-10 19:39:54 +0900

    マップ完成・制限時間完成
    頑張ったよ俺！

---
+++ ProjectV/Assets/scripts/BombTimer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BombTimer : MonoBehaviour {
+	
+	public  float Timer = 60.0f;
+	public Text timeText;
+	public GameObject CanvasObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (FUELScript.isBomb) {
+			
+			timeText.enabled = true;
+
+			if (Timer > 0) {
+				Timer -= Time.deltaTime;
+				timeText.text = Timer.ToString ();
+				
+				//Debug.Log (Timer);
+			} else {
+				
+				Application.LoadLevel ("GameSelect");
+			}
+		} else {
+			timeText.enabled = false;
+		}
+
+	}
+}
--- ProjectV/Assets/scripts/Object/FUELScript.cs
+++ ProjectV/Assets/scripts/Object/FUELScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class FUELScript : MonoBehaviour {
 	
+	public static bool isBomb = false;
+	
 	public static int getFUEL_01;
 	public static int getFUEL_02;
 	public static int getFUEL_03;
@@ -10,10 +12,21 @@ public class FUELScript : MonoBehaviour {
 	public static int getFUEL_05;
 	public static int getFUEL_06;
 	public static int getFUEL_07;
+	
+	private GameObject Gate01;
+	private GameObject Gate02;
+	private GameObject AllSaveObject;
 
 	// Use this for initialization
 	void Start () {
 		
+		Gate01 = GameObject.Find ("Gate01");
+		Gate02 = GameObject.Find ("Gate02");
+		AllSaveObject = GameObject.Find ("Savers");
+
+		// Debug用
+		// PlayerPrefs.SetInt (NowDataNumberScript.nowSaveData + "FUEL_06", 1);
+
 		getFUEL_01 = PlayerPrefs.GetInt (NowDataNumberScript.nowSaveData + "FUEL_01");
 		getFUEL_02 = PlayerPrefs.GetInt (NowDataNumberScript.nowSaveData + "FUEL_02");
 		getFUEL_03 = PlayerPrefs.GetInt (NowDataNumberScript.nowSaveData + "FUEL_03");
@@ -25,11 +38,18 @@ public class FUELScript : MonoBehaviour {
 		if (PlayerPrefs.GetInt (NowDataNumberScript.nowSaveData + this.name) == 1) {
 			GameObject.Destroy(gameObject);
 		}
+
+		if( getFUEL_01==1 && getFUEL_02==1 && getFUEL_03==1 && getFUEL_04==1 && getFUEL_05==1 && getFUEL_06==1 ){
+			
+			GameObject.Destroy(Gate01);
+			Debug.Log("All FUEL GET");
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnTriggerEnter2D(Collider2D coll){
@@ -45,6 +65,21 @@ public class FUELScript : MonoBehaviour {
 			if( this.name == "FUEL_07" ){ getFUEL_07 = 1; }
 
 			GameObject.Destroy(gameObject);
+
+			if( getFUEL_01==1 && getFUEL_02==1 && getFUEL_03==1 && getFUEL_04==1 && getFUEL_05==1 && getFUEL_06==1 ){
+
+				GameObject.Destroy(Gate01);
+				Debug.Log("All FUEL GET");
+			}
+
+			if( getFUEL_07 == 1 ){
+				
+				GameObject.Destroy(Gate02);
+				GameObject.Destroy(AllSaveObject);
+
+				isBomb = true;
+			}
 		}
 	}
+
 }
