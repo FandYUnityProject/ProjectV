bd3d8ec550334aedb13fe7ffcd14476d07db00d6
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-10 12:34:36 +0900

    Revert 53a7a5a..87e5dc8
    This rolls back to commit 53a7a5aa0dc5dade7777c513065ff6ee963567f9.

--- ProjectV/Assets/scripts/GravityInversion2D.cs
+++ ProjectV/Assets/scripts/GravityInversion2D.cs
@@ -13,10 +13,6 @@ public class GravityInversion2D : MonoBehaviour {
 	public bool isGravityInversion = false;	// 重力反転中か（接地するまで重力反転は１度のみ）
 
 	public Vector2 Gravityparameta = new Vector2(0,20);
-	public float positivePlayerScale_Y;
-	public float negativePlayerScale_Y;
-
-	bool isTouch;
 	
 	// Update is called once per frame
 	void Update () {
@@ -36,26 +32,12 @@ public class GravityInversion2D : MonoBehaviour {
 
 	public void OnTriggerExit2D(Collider2D coll){
 		Vector2 scale = transform.localScale;
-		if (coll.gameObject.tag == "Floor" && isTouch) {
-			scale.y = negativePlayerScale_Y;
+		if (coll.gameObject.tag == "Floor") {
 			scale.y = -1;
 			transform.localScale = scale;
 		}
-		if (coll.gameObject.tag == "Roof" && isTouch) {
-			scale.y = positivePlayerScale_Y;
-			transform.localScale = scale;
-			isTouch = false;
-		}
-	}
-
-	public void OnTriggerEnter2D(Collider2D coll){
-		Vector2 scale = transform.localScale;
-		if (coll.gameObject.tag == "Floor" && !isTouch) {
-			scale.y = positivePlayerScale_Y;
-			transform.localScale = scale;
-		}
-		if (coll.gameObject.tag == "Roof" && !isTouch) {
-			scale.y = negativePlayerScale_Y;
+		if (coll.gameObject.tag == "Roof") {
+			scale.y = 1;
 			transform.localScale = scale;
 		}
 	}
--- ProjectV/Assets/scripts/MoveController.cs
+++ ProjectV/Assets/scripts/MoveController.cs
@@ -9,9 +9,6 @@ public class MoveController : MonoBehaviour {
 	private Animator anim;
 	private bool isMove = false;
 	private Transform playerTransform;
-	public Vector2 scale;//= playerTransform.localScale;
-	public float positivePlayerScale_X;
-	public float negativePlayerScale_X;
 
 	void Start(){
 		//rb = GetComponent<Rigidbody2D> ();
@@ -21,14 +18,14 @@ public class MoveController : MonoBehaviour {
 
 	void Update(){
 		float horizontalMove = Input.GetAxisRaw ("Horizontal");
-		scale = playerTransform.localScale;
+		Vector2 scale = playerTransform.localScale;
 
 		if (horizontalMove > 0f) {
 			isMove = true;
-			scale.x = negativePlayerScale_X;
+			scale.x = -1f;
 		} else if (horizontalMove < 0f) {
 			isMove = true;
-			scale.x = positivePlayerScale_X;
+			scale.x = 1f;
 		} else {
 			isMove =false;
 		}
